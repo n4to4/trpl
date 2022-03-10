87ba3086560ce33dc164756257df4aca3b5dc4da
n4to4 <n4to4k@gmail.com>
2022-03-10 21:16:42 +0900

    rev

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,12 +1,5 @@
 fn main() {
-    let mut counter = 0;
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-
-    println!("the result is {}", result);
+    for n in (1..4).rev() {
+        println!("{n}");
+    }
 }
